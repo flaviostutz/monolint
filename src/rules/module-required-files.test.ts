3e2307f5943dbe289d0725fd25c8e682c5029fd8
tiagostutz <tiagostutz@gmail.com>
2022-10-21 18:46:00 +0200

    feat(tests): refs #4 better organize test case folder to have a reference

--- src/rules/module-required-files.test.ts
+++ src/rules/module-required-files.test.ts
@@ -3,12 +3,12 @@ import { loadBaseConfig } from '../utils/config';
 
 import rule from './module-required-files';
 
-const baseDir = 'src/rules/test-cases/module-requrired-files';
-
 describe('given a folder with non-strict config', () => {
 
+  const baseDir = 'src/rules/test-cases/module-requrired-files/non-strict';
+
   describe('when required files not found in folder', () => {
-    const baseConfig = loadBaseConfig(`${baseDir}/non-strict`, '.monolint.json');
+    const baseConfig = loadBaseConfig(`${baseDir}/mod-error-1`, '.monolint.json');
     const modules = discoverModules(baseDir, baseConfig);
 
     it('then should return error', async () => {
@@ -23,6 +23,9 @@ describe('given a folder with non-strict config', () => {
   });
 
   describe('when files in folder beyond the required', () => {
+    const baseConfig = loadBaseConfig(`${baseDir}/mod-success-1`, '.monolint.json');
+    const modules = discoverModules(baseDir, baseConfig);
+
     it('then should return success', async () => {
       const results = rule.checkModules(modules, baseDir);
       expect(results).toHaveLength(6);
@@ -32,17 +35,20 @@ describe('given a folder with non-strict config', () => {
         expect(results[0].valid).toBeFalsy();
       }
     });
+  });
 
-    describe('when files in folder exactly the required', () => {
-      it('then should return success', async () => {
-        const results = rule.checkModules(modules, baseDir);
-        expect(results).toHaveLength(6);
-        if (results) {
-          expect(results[0].resource.includes('package.json')).toBeTruthy();
-          expect(results[0].module?.name).toEqual('mod1-js');
-          expect(results[0].valid).toBeFalsy();
-        }
-      });
+  describe('when files in folder exactly the required', () => {
+    const baseConfig = loadBaseConfig(`${baseDir}/mod-success-2`, '.monolint.json');
+    const modules = discoverModules(baseDir, baseConfig);
+
+    it('then should return success', async () => {
+      const results = rule.checkModules(modules, baseDir);
+      expect(results).toHaveLength(6);
+      if (results) {
+        expect(results[0].resource.includes('package.json')).toBeTruthy();
+        expect(results[0].module?.name).toEqual('mod1-js');
+        expect(results[0].valid).toBeFalsy();
+      }
     });
 
   });
