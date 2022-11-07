f3969800b690321b1bbbc394e69515fed49c28cc
Flores Junior, S.E. (Sergio) <sergio.flores@nn.nl>
2022-11-07 20:15:05 +0100

    chore: start move to monorepo

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/no-commonjs
 module.exports = {
+  root: true,
   parserOptions: {
     // needed by some typescript rules
     project: ['./tsconfig.eslint.json'],
