4eef632f05ac747cdbbab336d7441a5929c85b56
Flávio Stutz <flaviostutz@gmail.com>
2023-01-21 05:36:38 +0100

    cleaning up boilerplate

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
+// eslint-disable-next-line import/no-commonjs
 module.exports = {
   parserOptions: {
-    // needed by some typescript rules
-    project: ['./tsconfig.eslint.json'],
+    project: ['./tsconfig.json'],
     tsconfigRootDir: __dirname,
   },
   // ignorePatterns: ['dist/**', 'build/**', 'coverage/**', 'node_modules/**', 'rules.md'],
